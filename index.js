aac63b58377005060076f40fcb65dd47ff0dd92a
Emily Lam <hlam93@gmail.com>
2018-04-03 21:31:01 -0700

    adding index

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const path = require('path')
 var request = require('request')
 var _ = require('lodash')
+const ejsLint = require('ejs-lint')
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send('hello emily'))
+  .get('/', (req, res) => res.send('hello em!'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
